df0bc1422422c087b543f78fe0ed169ea1022abd
Varun Jain <varun.jain@tala.co>
2019-11-04 18:20:55 +0530

    added login screen

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", function() {
+
   console.log("Hello World!");
 });
+naviBoard.setNavigation('loginform');
+console.log('hello123');
+function myFunction() {
+  alert('button clicked');
+}
