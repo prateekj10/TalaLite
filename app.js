d087cf51a24ecc2eab0940e4dbf1b179a1edfed3
Varun Jain <varun.jain@tala.co>
2019-11-04 20:34:43 +0530

    adding survey and loan decision dummy screens

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", function() {
-
   console.log("Hello World!");
 });
-naviBoard.setNavigation('loginform');
-console.log('hello123');
-function myFunction() {
-  alert('button clicked');
-}
+
+function myFunc() {
+  alert('hello world!');
+};
+
+function loanDecision() {
+
+};
